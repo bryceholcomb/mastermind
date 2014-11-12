b0885f30c5df9659d0123546d224589d80b4ca44
Bryce Holcomb <bryce_holcomb@outlook.com>
2014-11-12 11:39:10 -0700

    finish refactoring feedback messages, now all contained in printer

--- lib/game.rb
+++ lib/game.rb
@@ -5,7 +5,7 @@ require_relative './code_comparer'
 require_relative './hidden_code'
 
 class Game
-  attr_reader :instream, :outstream, :guesses, :guess, :formatted_guess
+  attr_reader :instream, :outstream, :guesses, :guess, :formatted_guess, :hidden_code
 
   def initialize(instream, outstream, possible_colors = %w(R G B Y))
     @hidden_code     = HiddenCode.new.reveal
@@ -31,13 +31,13 @@ class Game
     when exit? then puts @printer.exit_game_message
     when match?
       @timer.calculate_time_elapsed
-      puts "Congratulations! You guessed the sequence '#{@hidden_code.join}' in #{@guesses.count} guess(es) over #{@timer.minutes} minute(s), #{@timer.seconds} second(s).\n"
-    when Validator.too_short?(@guess) then puts @printer.too_short_message
-    when Validator.too_long?(@guess) then puts @printer.too_long_message
-    when Validator.contains_invalid_characters?(@formatted_guess, @possible_colors) then puts @printer.not_a_valid_guess
+      puts @printer.print_stats(hidden_code, guesses, @timer)
+    when Validator.too_short?(guess) then puts @printer.too_short_message
+    when Validator.too_long?(guess) then puts @printer.too_long_message
+    when Validator.contains_invalid_characters?(formatted_guess, @possible_colors) then puts @printer.not_a_valid_guess
     else
       @guesses << @guess
-      puts "'#{@guess.upcase}' has #{CodeComparer.correct_colors(@hidden_code, @formatted_guess)} of the correct elements with #{CodeComparer.correct_positions(@hidden_code, @formatted_guess)} in the correct position\nYou've taken #{@guesses.count} guess(es). Guess again!"
+      puts @printer.feedback(guess, hidden_code, formatted_guess, guesses)
     end
   end
 
@@ -47,7 +47,7 @@ class Game
   end
 
   def match?
-    CodeComparer.match?(@hidden_code, @formatted_guess)
+    CodeComparer.match?(hidden_code, formatted_guess)
   end
 
   def exit?
